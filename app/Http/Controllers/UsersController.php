5c11fe2d8415ca0a6d7a4b865bfd0f9bc33fa360
Valadez Martinez <d.valadez.martinez@accenture.com>
2018-09-13 09:17:25 -0500

    Hashing password again

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,35 +26,37 @@ class UsersController extends Controller
     public function store(){
         // go back and check Refactoring video
         $input = Input::all();
-        $confirmationCode = str_random(8);
-        $this->user->fill($input);
+        $confirm_code = str_random(8);
+        //$this->user->fill($input);
+        $this->user->email = Input::get('email');
+        $this->user->password = Hash::make(Input::get('password'));
+        $this->user->confirm_code = $confirm_code;
         $email = $this->user->email;
         //$value = config('mail');
         //info($value);
         if(! $this->user->isValid($input))
 		{
-            return Redirect::back()->withInput(Input::all())->withErrors($this->user->errors);
+            return Redirect::back()->withInput($input)->withErrors($this->user->errors);
             //return ($this->user->errors);
         }
         
         $searchResult = DB::select("CALL getEmailExists('".$email. "')");
-        //return $searchResult;
         if (empty($searchResult))
             $this->user->save();
         else {
             $errors = array('email'=> array('Email is already taken.'));
-            return Redirect::back()->withInput(Input::all())->withErrors($errors);
+            return Redirect::back()->withInput($input)->withErrors($errors);
         }
 
-        $data = array('email' => $email, 'confirmationCode' => $confirmationCode);
-        Mail::send('emails.confirm', $data, function ($message)
+        $data = array('email' => $email, 'confirmationCode' => $confirm_code);
+        /*Mail::send('emails.confirm', $data, function ($message)
         {
 
             $message->from('admin@vgmrad.io', 'Daniela Valadez');
 
             $message->to('8102c404a3-6ace2c@inbox.mailtrap.io');
 
-        });
+        });*/
 
         return Redirect::route('users.index');
     }
